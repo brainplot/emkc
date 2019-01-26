cd9531f8eb4d6b9ec233476ecffcd90e18081a21
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2019-01-26 09:53:10 -0500

    Started endpoint for getting topusers

--- platform/api/controllers/api/v1/stats/DiscordController.js
+++ platform/api/controllers/api/v1/stats/DiscordController.js
@@ -128,6 +128,70 @@ module.exports = {
             });
     },
 
+    topusers(req, res) {
+        var { start, end, limit } = req.query;
+
+        /*var query = {
+            where: {
+                $and: []
+            },
+            attributes: [
+                'channel',
+                [db.sequelize.literal('count(*)'), 'messages'],
+            ],
+            order: [
+                [db.sequelize.col('messages'), 'desc']
+            ],
+            group: [
+                'channel'
+            ],
+            limit: 1000
+        };
+
+        var dates = [];
+
+        if (user) {
+            query.where.user = {
+                $like: '%' + user.replace(/[^\x00-\x7F]/g, '') + '%'
+            };
+        }
+
+        if (start) {
+            start = moment(start);
+
+            if (start.isValid()) {
+                query.where.$and.push({
+                    created_at: {
+                        $gte: start.format('YYYY-MM-DD HH:mm:ss')
+                    }
+                });
+            }
+        }
+
+        if (end) {
+            end = moment(end);
+
+            if (end.isValid()) {
+                query.where.$and.push({
+                    created_at: {
+                        $lte: end.format('YYYY-MM-DD HH:mm:ss')
+                    }
+                });
+            }
+        }
+
+        if (limit > 0 && limit <= 1000) {
+            query.limit = +limit;
+        }*/
+
+        return db.discord_chat_messages
+            .find_all()
+            .then(stats => {
+                return res.send(stats);
+            });
+    },
+
     _config: {}
 
 };
+
